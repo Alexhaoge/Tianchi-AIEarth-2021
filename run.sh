6e01643358a0653ae530c46556b489fce72b2c57
Alexhaoge <1810064@mail.nankai.edu.cn>
2021-02-22 01:36:10 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 mkdir result
 mkdir output
-python main.py -e 100 --loss rmse --val-loss score -l 0.01
+python main.py -e 100 --loss rmse --val-loss score -l 0.05 --small-dataset=500 -p 30
 # python main.py --infer
 python zip.py
