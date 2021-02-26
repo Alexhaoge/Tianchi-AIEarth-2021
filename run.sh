2dbfb8d922c19421d9b22912982ce9ba703240f1
Alexhaoge <1810064@mail.nankai.edu.cn>
2021-02-26 13:54:28 +0800

    2.26交一发试试稳定性

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 mkdir result
 mkdir output
-python main.py -e 200 -l 0.01 -b 256 --loss rmse --val-loss score --refit
+python main.py -e 200 -l 0.01 -b 1024 --loss rmse --val-loss score --refit
 # python main.py --infer
 python zip.py
